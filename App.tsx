bb9130925e268aa01a321377024eef2a39989190
Rishicreates <122658430+rishiicreates@users.noreply.github.com>
2025-01-20 12:27:22 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import LocalMarketplace from './local-marketplace';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the App</h1>
+      <LocalMarketplace />
+    </div>
+  );
+}
+
+export default App;
